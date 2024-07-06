1b08f91210bf52b5491cddbf802489991979f4fb
Saheed <saolabram@gmail.com>
2024-07-06 10:37:01 +0100

    comment default route

--- routes/api.php
+++ routes/api.php
@@ -4,8 +4,8 @@ use App\Http\Controllers\BasicController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+// Route::get('/user', function (Request $request) {
+//     return $request->user();
+// })->middleware('auth:sanctum');
 
 Route::get('/greet', [BasicController::class, 'index']);
